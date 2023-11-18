39bec65281fd10040622495c38a5c45f979373fe
tkwk9 <tim.k.kwak@gmail.com>
2023-11-17 20:15:12 -0500

    get kv working in prod

--- functions/kv_example.js
+++ functions/kv_example.js
@@ -1,35 +1,18 @@
-export function onRequest(context) {
-  return new Response(JSON.stringify({
-    data: "wtf"
-  }))
+export async function onRequest(context) {
+  const { env, request } = context;
+
+  try {
+    const value = await env.KV_TEST.get("key");
+
+    if (value === null) {
+      return new Response("Key not found", { status: 404 });
+    }
+
+    return new Response(value, {
+      headers: { 'Content-Type': 'application/json' },
+    });
+  } catch (error) {
+    console.log(error);
+    return new Response('Error fetching data', { status: 500 });
+  }
 }
-
-
-
-// export function onRequest(context) {
-//   // Destructuring for easy access to necessary objects
-//   const { env, request } = context;
-//   return new Response(
-//     JSON.stringify({
-//       data: 1,
-//     })
-//   );
-
-//   // try {
-//   //   // Fetch data from KV store
-//   //   const value = await env.MY_KV_NAMESPACE.get("my-key");
-
-//   //   // Check if value exists
-//   //   if (value === null) {
-//   //     return new Response("Key not found", { status: 404 });
-//   //   }
-
-//   //   // Return the value as a response
-//   //   return new Response(value, {
-//   //     headers: { 'Content-Type': 'application/json' },
-//   //   });
-//   // } catch (error) {
-//   //   // Handle any errors
-//   //   return new Response('Error fetching data', { status: 500 });
-//   // }
-// }
