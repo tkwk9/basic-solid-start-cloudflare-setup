2ff1da2da01dc5744a7d3c4894b1fdb15929f816
tkwk9 <tim.k.kwak@gmail.com>
2023-11-17 22:48:17 -0500

    brute force SSR KV fetch

--- src/routes/xyz.jsx
+++ src/routes/xyz.jsx
@@ -1,11 +1,24 @@
 import { Title } from "solid-start";
+import { createResource, Show } from "solid-js";
 import styles from "./xyz.module.scss";
 
+const fetchKVData = async () => {
+  const response = await fetch("https://basic-solid-start-cloudflare-setup.pages.dev/kv_example");
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return response.json();
+};
+
 const Home = () => {
+  const [data] = createResource(fetchKVData);
+
   return (
     <main>
       <Title>SolidStart - XYZ</Title>
-      <h1 class={styles.header}>XYZ</h1>
+      <Show when={data()}>
+        <h1 class={styles.header}>{data().data}</h1>
+      </Show>
     </main>
   );
 };
