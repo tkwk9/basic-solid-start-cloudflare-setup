3521cd3e00acff8329cdcbd3f7d27eeb63a70176
tkwk9 <tim.k.kwak@gmail.com>
2023-11-22 17:52:01 -0400

    use module scss for counter component

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,10 +1,10 @@
 import { createSignal } from "solid-js";
-import "./Counter.scss";
+import styles from "./Counter.module.scss";
 
 const Counter = () => {
   const [count, setCount] = createSignal(0);
   return (
-    <button class="increment" onClick={() => setCount(count() + 1)}>
+    <button class={styles.increment} onClick={() => setCount(count() + 1)}>
       Clicks: {count()}
     </button>
   );
