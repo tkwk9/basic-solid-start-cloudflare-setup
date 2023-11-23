c04237565145c3fe777bd190564fc918cebd75ce
tkwk9 <tim.k.kwak@gmail.com>
2023-11-23 18:01:26 -0400

    comment + cleanup

--- run_dev.sh
+++ run_dev.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
+# 2 is a file descriptor for stderr
+# 1 is a file descriptor for stdout
+# 2>&1 redircts stderr to where stdout is going 
 npm run ss:watch > /dev/null 2>&1 &
-pid1=$!
+ss_watch_pid=$!
 
 npm run pages:dev &
-pid2=$!
+pages_dev_pid=$!
 
 cleanup() {
-    kill $pid1
-    kill $pid2
+    kill $ss_watch_pid
+    kill $pages_dev_pid
     exit
 }
 
-# Execute the cleanup function whenever it receives the SIGINT signal.
+# Execute the cleanup function whenever it receives the SIGINT signal
 trap cleanup SIGINT
 
-wait $pid2
-wait $pid1
+wait $pages_dev_pid
+wait $ss_watch_pid
