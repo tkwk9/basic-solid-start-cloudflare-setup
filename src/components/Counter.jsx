764260546a42c0542452bb9f2d3210b655070986
tkwk9 <tim.k.kwak@gmail.com>
2023-11-26 11:08:17 -0500

    use tailwind for button

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,10 +1,12 @@
 import { createSignal } from "solid-js";
-import styles from "./Counter.module.scss";
 
 const Counter = () => {
   const [count, setCount] = createSignal(0);
   return (
-    <button class={styles.increment} onClick={() => setCount(count() + 1)}>
+    <button
+      class="bg-blue-200 hover:bg-blue-300 text-blue-900 rounded-full border-solid border-2 border-blue-200 focus:border-blue-700 py-4 px-8"
+      onClick={() => setCount(count() + 1)}
+    >
       Clicks: {count()}
     </button>
   );
