040770e9202f62f5f0c6fff9670a525491e7f46b
tkwk9 <tim.k.kwak@gmail.com>
2023-11-28 18:14:52 -0800

    add prod CSR fallback

--- src/utils/data-fetchers.jsx
+++ src/utils/data-fetchers.jsx
@@ -1,9 +1,10 @@
 import { useServerContext } from "solid-start";
-import { createResource, onMount } from 'solid-js';
+import { createResource, onMount } from "solid-js";
 import { isServer } from "solid-js/web";
 
 const fetchKvData__dev = async (key) => {
-  if(isServer) return null;
+  if (isServer) return null;
+
   const response = await fetch(`/kv?key=${key}`);
   if (!response.ok) {
     throw new Error("Failed to fetch data");
@@ -11,13 +12,24 @@ const fetchKvData__dev = async (key) => {
   return response.json();
 };
 
+const useKvData__dev = (key) => {
+  // TODO: handle null key
+  let [data, { refetch }] = createResource(() => fetchKvData__dev(key));
+
+  onMount(() => {
+    refetch();
+  });
+
+  return [data];
+};
+
+// TODO: This still needs to handle SSR error better
 const fetchKvData__prod = async (key) => {
   if (isServer) {
     const { env } = useServerContext();
     const val = await env?.KV_TEST?.get(key);
-    // TODO: This probably shouldn't throw an error the way it is right now
-    // It should default to client-side fetch if there's an SSR error
-    if (!val) throw new Error("Failed to fetch data SSR");
+    
+    if (val) return { shouldRefetch: true };
     return JSON.parse(val);
   }
 
@@ -29,20 +41,18 @@ const fetchKvData__prod = async (key) => {
   return response.json();
 };
 
-const fetchKvData =
-  process.env.NODE_ENV === "development" ? fetchKvData__dev : fetchKvData__prod;
-
-
-const useKvData = (key) => {
+const useKvData__prod = (key) => {
   // TODO: handle null key
-  let [data, { refetch }] = createResource(() => fetchKvData(key));
+  let [response, { refetch }] = createResource(() => fetchKvData__prod(key));
 
-  // TODO: Make this dev-specific + handle SSR error in production
   onMount(() => {
-    refetch();
+    if (response()?.shouldRefetch) refetch();
   });
 
-  return [data];
-}
+  return [response];
+};
+
+const useKvData =
+  process.env.NODE_ENV === "development" ? useKvData__dev : useKvData__prod;
 
 export { useKvData };
